351f26d5e2796e96588fb64d48dbebef4334ea84
Luis Dasta <ldasta786@gmail.com>
2018-12-22 19:49:57 -0600

    Fresh spring initializr

---
+++ src/main/java/com/webapi/kotlinWebApi/KotlinWebApiApplication.java
@@ -0,0 +1,14 @@
+package com.webapi.kotlinWebApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinWebApiApplication.class, args);
+	}
+
+}
+
